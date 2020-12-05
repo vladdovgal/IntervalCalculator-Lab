46a2b620273bac4a0c61652eb11b547a0326b1fb
Vladyslav_Dovhal <Vladyslav_Dovhal@epam.com>
2020-12-06 01:51:34 +0200

    implement logic of arithmetic operations on intervals

--- app/src/main/java/com/example/intervalcalculator/ui/home/Part1Fragment.kt
+++ app/src/main/java/com/example/intervalcalculator/ui/home/Part1Fragment.kt
@@ -1,17 +1,20 @@
 package com.example.intervalcalculator.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.intervalcalculator.R
 import com.example.intervalcalculator.databinding.FragmentPart1Binding
+import com.example.intervalcalculator.utils.IntervalUtils
 
-class Part1Fragment : Fragment() {
+const val LOG_TAG = "myLogs"
+class Part1Fragment : Fragment(R.layout.fragment_part1) {
 
     private lateinit var binding: FragmentPart1Binding
+    private var utils: IntervalUtils = IntervalUtils()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,17 +22,19 @@ class Part1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentPart1Binding.inflate(layoutInflater)
-        val root = inflater.inflate(R.layout.fragment_part1, container, false)
-        return root
+        return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        initUI()
+        binding.btnFindSolution.setOnClickListener {
+            Log.d(LOG_TAG, "chck" )
+            findSolution()
+        }
         super.onViewCreated(view, savedInstanceState)
     }
 
-    private fun initUI() {
+    private fun findSolution() {
         var a0 = 0.0
         var a1 = 0.0
         var b0 = 0.0
@@ -40,5 +45,11 @@ class Part1Fragment : Fragment() {
             b0 = etInt2Start.text.toString().toDouble()
             b1 = etInt2End.text.toString().toDouble()
         }
+        val intervalA = Pair(a0, a1)
+        val intervalB = Pair(b0, b1)
+        Log.d(LOG_TAG, "A + B = ${utils.add(intervalA, intervalB)}" )
+        Log.d(LOG_TAG, "A - B = ${utils.subtract(intervalA, intervalB)}" )
+        Log.d(LOG_TAG, "A * B = ${utils.multiply(intervalA, intervalB)}" )
+        Log.d(LOG_TAG, "A / B = ${utils.divide(intervalA, intervalB)}" )
     }
 }
--- app/src/main/java/com/example/intervalcalculator/utils/IntervalUtils.kt
+++ app/src/main/java/com/example/intervalcalculator/utils/IntervalUtils.kt
@@ -2,20 +2,42 @@ package com.example.intervalcalculator.utils
 
 class IntervalUtils {
 
-    fun add(interval1: Interval, interval2: Interval): Interval {
-        return Pair(
-            interval1.first + interval2.first,
-            interval1.second + interval2.second
-        )
-    }
+    fun add(interval1: Interval, interval2: Interval): Interval =
+        interval1.sum(interval2)
 
-    fun subtract(interval1: Interval, interval2: Interval): Interval {
-        return Pair(
-            interval1.first - interval2.first,
-            interval1.second - interval2.second
-        )
-    }
+    fun subtract(interval1: Interval, interval2: Interval): Interval =
+        interval1.difference(interval2)
 
+    fun multiply(interval1: Interval, interval2: Interval): Interval =
+        interval1.product(interval2)
+
+    fun divide(interval1: Interval, interval2: Interval): Interval =
+        interval1.product(Pair(1 / interval2.first, 1 / interval2.second))
 }
 
 typealias Interval = Pair<Double, Double>
+
+/** Cartesian product
+ * ["a0","a1"].cartesianProduct( ["b0", "b1"] )  --> [(a0, b0), (a0, b1), (a1, b0), (a1, b1)]
+ */
+private fun <T, S> List<S>.cartesianProduct(other: List<T>) = this.flatMap {
+    List(other.size) { i -> Pair(it, other[i]) }
+}
+
+private fun Pair<Double, Double>.sum(otherPair: Pair<Double, Double>) = Pair(
+    this.first + otherPair.first, this.second + otherPair.second
+)
+
+private fun Pair<Double, Double>.difference(otherPair: Pair<Double, Double>) = Pair(
+    this.first - otherPair.second, this.second - otherPair.first
+)
+
+private fun Pair<Double, Double>.product(otherPair: Pair<Double, Double>): Pair<Double, Double> {
+    val listLeft = listOf(this.first, this.second)
+    val listRight = listOf(otherPair.first, otherPair.second)
+    val combinationsProduct = listLeft.cartesianProduct(listRight).map { it.first * it.second }
+    return Pair(
+        combinationsProduct.minByOrNull { it } ?: 0.0,
+        combinationsProduct.maxByOrNull { it } ?: 0.0
+    )
+}
